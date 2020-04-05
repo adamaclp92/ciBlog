89e1b6e15087807dab2b4f0d6624da0e1d7ca3b3
adamaclp92 <adamaclp92@gmail.com>
2020-04-05 18:15:40 +0200

    CodeIgniter tutorial_1

---
+++ application/controllers/Post.php
@@ -0,0 +1,10 @@
+<?php
+    class Pages extends CI_Controller{
+        public function index(){
+            $data['title'] = 'Latest Posts';
+
+            $this->load->view('templates/header');
+            $this->load->view('posts/index'.$page, $data);
+            $this->load->view('templates/footer');
+        }
+    }
---
+++ application/views/templates/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <head>
+        <title>ciBlog</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    </head> 
+    <body>
+        
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="/">ciBlog</a>
+
+  <div class="collapse navbar-collapse" id="navbarColor01">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="container">
